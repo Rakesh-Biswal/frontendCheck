b8f61e177dd652dddd66ec19633f5ee0a4991679
Rakesh-Biswal <rb2306114@gmail.com>
2024-05-24 11:04:31 +0530

    fuck u

--- register.js
+++ register.js
@@ -21,7 +21,7 @@ document.getElementById("registrationForm").addEventListener("submit", async (e)
     };
 
     try {
-        const response = await fetch(`/${apiUrl}/register`, {
+        const response = await fetch(`${apiUrl}/register`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
